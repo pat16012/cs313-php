f75e7b822de19d7cbbe5961cf994091d28e31578
exno <pat16012@byui.edu>
2019-01-19 21:29:04 -0700

    Finished
    Added text photos and layout

--- web/header.php
+++ web/header.php
@@ -1,8 +1,6 @@
 
 <ul>
-  <li><a href="index.php">Home</a></li>
-  <li><a href="page3.php">Login</a></li>
-  <li><a href="page2.php">Page 2</a></li>
-  <li><a href="page3.php">Page 3</a></li>
+  <li><a href="index.php">Winter</a></li>
+  <li><a href="page2.php">Summer</a></li>
  
 </ul>
