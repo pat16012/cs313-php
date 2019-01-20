9549c029422859d231602ee033ddc840412a8329
exno <pat16012@byui.edu>
2019-01-19 19:39:28 -0700

    Placment Pages
    Added Placment pages

---
+++ web/header.php
@@ -0,0 +1,8 @@
+
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="page3.php">Login</a></li>
+  <li><a href="page2.php">Page 2</a></li>
+  <li><a href="page3.php">Page 3</a></li>
+ 
+</ul>
