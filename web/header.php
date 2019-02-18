728fb99c53c55ae8498fa85186200dc020bf0a13
Justin Paton <pat16012@byui.edu>
2019-02-18 14:09:21 -0700

    updated php

--- web/header.php
+++ web/header.php
@@ -3,5 +3,7 @@
     <li><a href="index.php">Winter</a></li>
     <li><a href="summer.php">Summer</a></li>
     <li><a href="store.php">Store</a></li>
+    <li><a href="databasePHP.php">Database</a></li>
+    
  
 </ul>
