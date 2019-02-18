d9cefa0f0cc9c57247cdd4594eda0cf4d7255e0d
Justin Paton <pat16012@byui.edu>
2019-02-18 16:04:40 -0700

    cleaned up code

--- web/dbaccess.php
+++ web/dbaccess.php
@@ -17,7 +17,7 @@ function get_db(){
         // Create the PDO Connection String
         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
         
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $ex)
     {
