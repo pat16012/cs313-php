63c586da822ea2cf26114301d6b8f12aac5e2527
Justin OPaton <pat16012@byui.edu>
2019-02-20 14:02:55 -0700

    updated DB UrL

--- web/dbaccess.php
+++ web/dbaccess.php
@@ -1,30 +1,35 @@
 <?php
-
-function get_db(){
-    $db = NULL;
-
-    try{
-        $dburl = getenv('DATABASE_URL');
-        
-        $dbOpts = parse_url($dbUrl);
-        
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
-
-        // Create the PDO Connection String
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-        
-       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $ex)
-    {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-    }
-
-    return $db;
+/**********************************************************
+* File: dbConnect.php
+* Author: Br. Burton
+* 
+* Description: Shows how to connect using either local
+* OR Heroku credentials, depending on whether the code
+* is executing at heroku.
+***********************************************************/
+function get_db() {
+	$db = NULL;
+	try {
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('CS313_ENV');
+		
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+	return $db;
 }
-?>
